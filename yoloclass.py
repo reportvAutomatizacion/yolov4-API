e7270d2c88ea3fcb7755119f6101934a61213a77
nmontesino@reportv.onmicrosoft.com <reportv.automatizacion@gmail.com>
2022-03-15 11:41:49 -0300

    texto innecesario

--- yoloclass.py
+++ yoloclass.py
@@ -17,26 +17,6 @@ import numpy as np
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
-
-''' CON FLAGS
-flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
-flags.DEFINE_string('weights', './checkpoints/yolov4-416',
-                    'path to weights file')
-flags.DEFINE_integer('size', 416, 'resize images to')
-flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
-flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
-flags.DEFINE_list('images', './data/images/dog.jpg', 'path to input image')
-flags.DEFINE_string('output', './detections/', 'path to output folder')
-flags.DEFINE_float('iou', 0.45, 'iou threshold')
-flags.DEFINE_float('score', 0.50, 'score threshold')
-flags.DEFINE_boolean('count', False, 'count objects within images')
-flags.DEFINE_boolean('dont_show', False, 'dont show image output')
-flags.DEFINE_boolean('info', False, 'print info on detections')
-flags.DEFINE_boolean('crop', False, 'crop detections from images')
-flags.DEFINE_boolean('ocr', False, 'perform generic OCR on detection regions')
-flags.DEFINE_boolean('plate', False, 'perform license plate recognition')
-'''
-
 print('CARGANDO MODELO')
 director = './checkpoints/yolov4-416'
 config = ConfigProto()
